6895c3d4d8711078ff19080a510de484a854c5d8
Leonardo <leosimoes.engcomp@gmail.com>
2023-11-01 13:29:40 -0300

    Update project - fix package

---
+++ ProcessamentoDados/ColetorDados.py
@@ -0,0 +1,43 @@
+#Autor: Leonardo Simões
+
+import streamlit as st
+import pandas as pd
+import base64
+
+class ColetorDados:
+    def __init__(self):
+        self.sem_rotulos = False
+        self.separador = ','
+
+    def verificar_rotulo(self):
+        if st.checkbox('O arquivo não possui rótulos para colunas na primeira linha'):
+            self.sem_rotulos = True
+
+    def verificar_separador(self):
+        sep_dict = {'vírgula': ',', 'ponto e vírgula': ';', 'um espaço': ' ','tabulação':'\t'}
+        sep = st.selectbox('Selecione o separador usado no arquivo', list(sep_dict.keys()))
+        if sep:
+            self.separador = sep_dict[sep]
+
+    def carregar_arquivo(self):
+        return st.file_uploader('Upload de arquivo csv, tsv ou txt:', type=['csv','tsv','txt'])
+
+    @st.cache
+    def carregar_dados(self, arquivo):
+        if self.sem_rotulos:
+            df = pd.read_csv(arquivo, sep=self.separador, header=None)
+            df.columns = ['x' + str(i) for i in range(1, df.shape[1] + 1)]
+            return df
+        else:
+            df = pd.read_csv(arquivo, sep=self.separador)
+            lowercase = lambda x: str(x).lower()
+            df.rename(lowercase, axis='columns', inplace=True)
+            return df
+
+    def download_df_link(self, df):
+        csv = df.to_csv(index=False)
+        b64 = base64.b64encode(
+            csv.encode()
+        ).decode()
+        return f'<a href="data:file/csv;base64,{b64}" download="dados_limpos.csv">Download do dataset limpo</a>'
+
--- ProcessamentoDados/Regressor.py
+++ ProcessamentoDados/Regressor.py
@@ -1,7 +1,6 @@
 #Autor: Leonardo Simões
 
 import streamlit as st
-import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import LogisticRegression
@@ -22,13 +21,13 @@ class Regressor:
                     reg = LinearRegression()
                     reg.fit(self.df[X], self.df[[y]])
                     st.write('Score do treinamento: ' + str(round(reg.score(self.df[X], self.df[[y]]),3)))
+
                     #Formatando coeficientes para impressao
-                    coeficientes_valores = np.append(reg.coef_, reg.intercept_)
-                    coeficientes_df = pd.DataFrame(coeficientes_valores).T
-                    coeficientes_nomes = X.copy()
-                    coeficientes_nomes.append('constante')
-                    coeficientes_df.columns = coeficientes_nomes
-                    coeficientes_df.index = ['Coeficientes']
+                    coeficientes_valores = reg.coef_[0].tolist() + [reg.intercept_[0]]
+                    coeficientes_nomes = X.copy() + ['constante']
+                    coeficientes_df = pd.DataFrame([coeficientes_valores], columns=coeficientes_nomes,
+                                                   index=['Coeficientes'])
+
                     st.table(coeficientes_df)
 
 
@@ -44,11 +43,11 @@ class Regressor:
                     clf = LogisticRegression(random_state=0)
                     clf.fit(self.df[X], self.df[[y]])
                     st.write('Score do treinamento: ' + str(round(clf.score(self.df[X], self.df[[y]]),3)))
+
                     #Formatando coeficientes para impressao
-                    coeficientes_valores = np.append(clf.coef_, clf.intercept_)
-                    coeficientes_df = pd.DataFrame(coeficientes_valores).T
-                    coeficientes_nomes = X.copy()
-                    coeficientes_nomes.append('constante')
-                    coeficientes_df.columns = coeficientes_nomes
-                    coeficientes_df.index = ['Coeficientes']
+                    coeficientes_valores = clf.coef_[0].tolist() + [clf.intercept_[0]]
+                    coeficientes_nomes = X.copy() + ['constante']
+                    coeficientes_df = pd.DataFrame([coeficientes_valores], columns=coeficientes_nomes,
+                                                   index=['Coeficientes'])
+
                     st.table(coeficientes_df)
